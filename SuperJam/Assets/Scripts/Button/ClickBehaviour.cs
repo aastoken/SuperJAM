1854fa193f5bb9cca0f5db05b13d7e64984db61e
Alvaro Tomas <alvarotl2105@gmail.com>
2019-05-04 00:16:31 +0200

    Created 2 scripts. Nothing done for now.

---
+++ SuperJam/Assets/Scripts/Button/ClickBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBehaviour : MonoBehaviour
+{
+    DoorState _actualState;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
