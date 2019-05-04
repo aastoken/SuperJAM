d260ef38fee504d9453811ee7494124b067e63a2
gabivlj <gabitriqui@gmail.com>
2019-05-05 01:32:48 +0200

    TextHandler

---
+++ SuperJam/Assets/TextHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class TextHandler : MonoBehaviour
+{
+    Text txt;
+    public GameManager gameManager;
+
+    void Start()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        txt.text = gameManager.score.ToString();
+    }
+
+}
