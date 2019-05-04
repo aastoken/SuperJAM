9df0d78a9dae17a27fb0e66ca044516f983d0f1a
Marutho <marutho97@gmail.com>
2019-05-04 12:19:51 +0200

    soundManagerSDSA

---
+++ SuperJam/Assets/Music.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip MainTheme1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.instance.PlaySingle(MainTheme1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
