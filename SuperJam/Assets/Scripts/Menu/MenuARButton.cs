c1b0271d1300b97698a061960bf3b9ea6483fea8
gabivlj <gabitriqui@gmail.com>
2019-06-04 20:03:45 +0200

    no message

---
+++ SuperJam/Assets/Scripts/Menu/MenuARButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MenuARButton : MonoBehaviour
+{
+    TextMeshPro tmp;
+
+    private void Awake()
+    {
+        tmp = GetComponent<TextMeshPro>();
+    }
+
+    private void OnMouseOver()
+    {
+        tmp.color = new Color32(255, 134, 124, 255);
+    }
+
+    private void OnMouseExit()
+    {
+        tmp.color = Color.white;
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameObject.tag == "ExitButton")
+        {
+            Debug.Log("Quitting game...");
+            Application.Quit();
+        }
+
+        if (gameObject.tag == "NewGameButton")
+        {
+            Debug.Log("Loading new game...");
+            SceneManager.LoadScene(2);
+            //SceneManager.LoadScene("MainScene");
+        }
+
+    }
+}
