c95043f1292c754449f17dd6e5de97a5616955fe
gabivlj <gabitriqui@gmail.com>
2019-06-04 22:37:48 +0200

    Mobile implementation and AR.

---
+++ SuperJam/Assets/Scripts/Mobile/GyroMobile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroMobile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Input.gyro.enabled = false;
+        if (IsMobile()) Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsMobile())
+        {
+            float rotx = Input.gyro.rotationRate.x;
+            float roty = Input.gyro.rotationRate.y;
+            //Vector3 gyroEulerAngles = gyroCurrentState.eulerAngles;
+            transform.eulerAngles = new Vector3(rotx, roty, 0);
+        }
+    }
+
+    bool IsMobile()
+    {
+        return Application.isMobilePlatform;
+    }
+}
